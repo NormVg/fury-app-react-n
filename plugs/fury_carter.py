6fc1c23ed5531c2ab9e8b9143bd5d5f4f7e65bcf
NormVg <87635928+NormVg@users.noreply.github.com>
2023-07-25 13:28:27 +0530

    Update fury_carter.py

--- plugs/fury_carter.py
+++ plugs/fury_carter.py
@@ -6,17 +6,10 @@ def ask_fury(com,user):
     response = requests.post("https://api.carterlabs.ai/chat", headers={
         "Content-Type": "application/json"
     }, data=json.dumps({
-        "text": f"{user}: {com}",
+        "text": f"{user} : {command}",
         "key": carter_key,
-        "playerId": "007" # THIS CAN BE ANYTHING YOU WANT!
-
+        "user_id": "Vishnu" 
     }))
-    force_behav = response.json()['forced_behaviours']
-    res = response.json()['output']["text"]
-#print(response.json())
-    if len(force_behav) >0:
-        return  res,force_behav[0]['name']
-    else:
-        return res, ""
+    return response.json()['output']['text']
 
 # print(ask_fury("tell space news fury",""))
